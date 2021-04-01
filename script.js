1bde9b85bd69e725945c83710772c3443600354f
Brandon Handoko <brandon.handoko@gmail.com>
2021-04-01 00:32:51 -0700

    Add dark theme toggle and new css

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+
+    window.onload=function(){
+        const themeToggle = document.querySelector("u");
+        themeToggle.addEventListener("click", function() {
+            document.body.classList.toggle("light-theme");
+        });
+    }
+    
+</script>
